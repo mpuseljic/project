f365449bc064891b7a59f261ada2674fdee02bf4
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-11-01 16:05:43 +0100

    set hex value

--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Square = ({ colorValue }) => {
+const Square = ({ colorValue, hexValue }) => {
   return (
     <section className="square" style={{ backgroundColor: colorValue }}>
       <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
     </section>
   );
 };
